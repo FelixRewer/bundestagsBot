6ee536dcc7e542383f7d3814444492615fbe0847
FelixRewer <39186283+FelixRewer@users.noreply.github.com>
2019-02-27 21:57:06 +0100

    Update warn.py again
    You can now warn multiples members at the same time.

--- commands/warn.py
+++ commands/warn.py
@@ -6,14 +6,18 @@ settings = {
 }
 
 async def main(client, message, params):
-    badbois = str(message.content)[5:].strip()
-    for member in client.get_all_members():
-        if member.mention == badbois:
-            badboi = member
-    warned = get(client.get_guild(531445761733296130).roles, name='ErsteVerwarnung') in badboi.roles
-    if not warned:
-        await message.channel.send(content=badboi.mention + ' verwarnt!')
-        punishrole = get(client.get_guild(531445761733296130).roles, id=533336650139435021)
-        await badboi.add_roles(punishrole)
-    else:
-        await message.channel.send(content='Benutzer wurde bereits einmal verwarnt!')
+    not_warned = []
+    punishrole = get(client.get_guild(531445761733296130).roles, id=533336650139435021)
+    
+    for member in message.mentions:
+        warned = punishrole in member.roles
+        if not warned:
+            await member.add_roles(punishrole)
+        else:
+            not_warned.append(member)
+    
+    await message.channel.send(content=len(message.mentions) + ' Benutzer verwarnt!' 
+                                       if len(not_warned) == 0 
+                                       else 'Benutzer ' +
+                                            ', '.join(map(lambda member: member.name, not_warned)) +
+                                            ' wurden bereits einmal verwarnt!')
